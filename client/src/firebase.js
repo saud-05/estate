dbafe325fbf17e80ad8ec46ca771a290cb012154
saud-05 <saudhussain003@gmail.com>
2024-10-01 00:05:39 +0400

    added

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,11 @@
 import { initializeApp } from 'firebase/app';
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "estate-41214.firebaseapp.com",
-  projectId: "estate-41214",
-  storageBucket: "estate-41214.appspot.com",
-  messagingSenderId: "1040194570553",
-  appId: "1:1040194570553:web:ecf7b691af1950003100e8",
-  measurementId: "G-7TDR9DKVV5"
+  authDomain: "realestate-fadf3.firebaseapp.com",
+  projectId: "realestate-fadf3",
+  storageBucket: "realestate-fadf3.appspot.com",
+  messagingSenderId: "609316131944",
+  appId: "1:609316131944:web:bf8411477eaf485c0217dd"
 };
 console.log("apiKey")
 export const app = initializeApp(firebaseConfig);
